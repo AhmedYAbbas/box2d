abdf86ee1335c7baea329eb8ec0ce2e6bacba24c
Ahmed Yasser <ahmedyaserabbas@gmail.com>
2025-05-12 18:16:22 +0300

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+project "box2d"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+
+		"inlcude/**.h"
+	}
+
+    includedirs
+    {
+        "include"
+    }
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++20"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
